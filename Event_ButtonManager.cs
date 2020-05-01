22db69ee5248ff246795f71dfadfd3c224574c08
sojae95 <37572033+sojae95@users.noreply.github.com>
2020-05-01 15:31:53 +0900

    Commit Code
    Code Commit

---
+++ Event_ButtonManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Event_ButtonManager : MonoBehaviour
+{
+    public GameObject Image;
+    public GameObject Audio;
+    
+
+    private float backVol = 1.0f;
+    // Start is called before the first frame update
+    public void ImageSetActive()
+    {
+        Image.SetActive(!Image.active);
+    }
+
+    public void AudioSetActive()
+    {
+        Audio.SetActive(!Audio.active);
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Event_Music.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Event_Music : MonoBehaviour
+{
+    public Slider BackVolume;
+    private AudioSource Audio;
+
+    public float BackVol = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Audio = this.gameObject.GetComponent<AudioSource>();
+        this.Audio.loop = false;
+        Audio.volume = BackVolume.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Audio.volume = BackVolume.value;
+        BackVol = BackVolume.value;
+    }
+}
